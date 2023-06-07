6a5a7e54f80db6f5ee4a35eb50401550b187c0f4
Sherwin Varkiani <s.varkiani@gmail.com>
2023-06-06 23:35:13 -0400

    got data and populate bingo table

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 // client/src/App.js
 
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -13,11 +12,59 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
+  console.log(data);
+
+  function display() {
+    if (data && data.length === 25) {
+      return (<div className="App">
+      <header className="App-header">
+        <table>
+          <tr>
+            <td>{data[0]}</td>
+            <td>{data[1]}</td>
+            <td>{data[2]}</td>
+            <td>{data[3]}</td>
+            <td>{data[4]}</td>
+          </tr>
+          <tr>
+            <td>{data[5]}</td>
+            <td>{data[6]}</td>
+            <td>{data[7]}</td>
+            <td>{data[8]}</td>
+            <td>{data[9]}</td>
+          </tr>
+          <tr>
+            <td>{data[10]}</td>
+            <td>{data[11]}</td>
+            <td>{data[12]}</td>
+            <td>{data[13]}</td>
+            <td>{data[14]}</td>
+          </tr>
+          <tr>
+            <td>{data[15]}</td>
+            <td>{data[16]}</td>
+            <td>{data[17]}</td>
+            <td>{data[18]}</td>
+            <td>{data[19]}</td>
+          </tr>
+          <tr>
+            <td>{data[20]}</td>
+            <td>{data[21]}</td>
+            <td>{data[22]}</td>
+            <td>{data[23]}</td>
+            <td>{data[24]}</td>
+          </tr>
+        </table>
+      </header>
+    </div>);
+    }
+    return (<div></div>)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        {display()}
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 // server/index.js
+var data = require('./data.json');
 
 const express = require("express");
 
@@ -6,10 +7,63 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-	res.json({ message: "Hello from server!" });
+const DataType = {
+  Food: 0,
+  Enemies: 1,
+};
+
+const Quantities = {
+  0: 1,
+  1: 3,
+  2: 5,
+  3: 10,
+};
+
+app.get("/api", async (req, res) => {
+  var bingoCells = new Set();
+  while (bingoCells.size < 25) {
+    bingoCells.add(getBingoSlot());
+  }
+
+  var arr = Array.from(bingoCells);
+
+	res.json({ message: arr });
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+function getRandomItem(dataList) {
+  let rnd = rng(dataList.length);
+
+  return dataList[rnd];
+}
+
+function rng(exclusiveMax) {
+  return Math.floor(Math.random() * exclusiveMax);
+}
+
+function getBingoSlot() {
+  var rnd = rng(getEnumLength(DataType));
+  var msg = "Hello from server!"
+
+  var quantity = Quantities[rng(getEnumLength(Quantities))]
+
+  switch(rnd) {
+    case DataType.Food:
+      msg = `Cook ${quantity} ${getRandomItem(data["food"])}`
+      break;
+    case DataType.Enemies:
+      msg = `Defeat ${quantity} ${getRandomItem(data["enemies"])}`
+      break;
+  }
+
+  msg += quantity > 1 ? "(s)" : ""; 
+
+  return msg;
+}
+
+function getEnumLength(enumName) {
+  return Object.keys(enumName).length
+}
